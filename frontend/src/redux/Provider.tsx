40d04fa79b20a3589624d8ebeb44b359061f6231
iamtalha19 <iamtalha134@gmail.com>
2026-03-05 12:49:09 +0500

    update

--- frontend/src/redux/Provider.tsx
+++ frontend/src/redux/Provider.tsx
@@ -2,14 +2,7 @@
 
 import { Provider } from "react-redux";
 import { store, persistor } from "@/redux/Store";
-import { PersistGate } from "redux-persist/integration/react";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return (
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        {children}
-      </PersistGate>
-    </Provider>
-  );
+  return <Provider store={store}>{children}</Provider>;
 }
