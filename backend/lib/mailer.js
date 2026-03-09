af6d3195c52ba0dac98e3178ee27cad44280ac91
iamtalha19 <iamtalha134@gmail.com>
2026-03-09 14:21:27 +0500

    update

--- backend/lib/mailer.js
+++ backend/lib/mailer.js
@@ -1,13 +1,17 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
+  host: "142.251.127.108",
   port: 465,
   secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  tls: { rejectUnauthorized: false, servername: "smtp.gmail.com" },
+  connectionTimeout: 20000,
+  greetingTimeout: 20000,
+  socketTimeout: 30000,
   family: 4,
 });
 
